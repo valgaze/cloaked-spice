e4d6df4ca52fbcc6d9874d9dad08269673fb2ae1
Victor Algaze <valgaze@gmail.com>
2013-12-30 11:36:32 -0800

    Added names to users, need to make password field have astericks on edit page, installed simple form gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
      Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.8.8-Q16'
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+       validates :image, presence: true
+  validates :description, presence: true
+
 end
